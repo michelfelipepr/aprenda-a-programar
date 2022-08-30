c7ff4ce370a819d250e92122c3402d9082463101
michelfelipepr <michelfelipepr@hotmail.com>
2022-08-30 14:26:40 -0300

    mensagem de commit

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,17 @@
+// Comentario de TESTES do CODIGOOO
+
+let nome
+nome = "Michel"
+
+let email
+email = "michelfelipepr@hotmail.com"
+
+let profissao
+profissao = "Supervisor"
+
+let idade
+idade = "35"
+
+let local
+local = "Nordeste"
+
