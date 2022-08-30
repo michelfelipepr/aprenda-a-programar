d75f7e24f831e718ff769ca4dc9503e4482285fe
michelfelipepr <michelfelipepr@hotmail.com>
2022-08-30 17:08:52 -0300

    mensagem de commit

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 1
+let n = 9
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let textoexibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log(textoexibicao)
+    i++
+ }
