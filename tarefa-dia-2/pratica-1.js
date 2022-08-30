e6d966df3f3067fc8adb570271f399faace0f717
michelfelipepr <michelfelipepr@hotmail.com>
2022-08-30 14:43:06 -0300

    mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,16 +2,21 @@
 
 let nome
 nome = "Michel"
+console.log(nome)
 
 let email
 email = "michelfelipepr@hotmail.com"
+console.log(email)
 
 let profissao
 profissao = "Supervisor"
+console.log(profissao)
 
 let idade
-idade = "35"
+idade = 35
+console.log(idade)
 
 let local
 local = "Nordeste"
+console.log(local)
 
